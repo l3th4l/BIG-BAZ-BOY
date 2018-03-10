37a507c481543c5cf9e698a6d19f8045c829e03b
l3th4l <safalyaglobal@gmail.com>
2018-03-11 01:03:55 +0530

    CamMove

--- Assets/Game Assets/Scripts/CameraMove.cs
+++ Assets/Game Assets/Scripts/CameraMove.cs
@@ -16,16 +16,8 @@ internal sealed class CameraMove : MonoBehaviour
     }
 
     private void Start()
-<<<<<<< HEAD:Assets/CameraMove.cs
     {  
 
         this.offset = this.transform.position - this.target.position;
     }
 }
-=======
-    {
-        this.transform.position = Vector3.Lerp(this.transform.position, this.target.position + this.offset, this.speed);
-        this.offset = this.transform.position - this.target.position;
-    }
-}
->>>>>>> d1767412494b0fca08e27ae8d6d90cbf32992802:Assets/Game Assets/Scripts/CameraMove.cs
