cc53b355a3f4fb2b55fdb15b33ac5796d165b985
l3th4l <safalyaglobal@gmail.com>
2018-03-11 20:16:19 +0530

    Bazooka changes

--- Assets/Game Assets/Scripts/Bazooka.cs
+++ Assets/Game Assets/Scripts/Bazooka.cs
@@ -14,12 +14,15 @@ internal sealed class Bazooka : MonoBehaviour
     [SerializeField]
     private Transform rocketSpawn;
 
+    [SerializeField]
+    private float dampFactor;
+
     public void Shoot()
     {
         var rocketRb = Bazooka.Instantiate(this.rocketPrefab, this.rocketSpawn.position, this.rocketSpawn.rotation).GetComponent<Rigidbody2D>();
         var force = this.rocketSpawn.up * this.forceMagnitude;
         rocketRb.AddForce(force);
-        this.rb.AddForce(-force);
+        this.rb.velocity = -force/dampFactor;
     }
 
     private void Reset()
